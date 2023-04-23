b1bd807ee2a935a1ddc81006278b81da62b6d019
Allyson <allysonmt@gmail.com>
2023-04-23 05:48:28 -0300

    Create CpfValidationRuleTest.php

---
+++ src/tests/Unit/Arch/App/Rules/CpfValidationRuleTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ArchCrudLaravel\Tests\Unit\Arch\App\Rules;
+
+use ArchCrudLaravel\App\Rules\CpfValidationRule;
+use Tests\TestCase;
+
+class CpfValidationRuleTest extends TestCase
+{
+    /**
+     * @dataProvider validCpfDataProvider
+     */
+    public function testValidCpf($cpf)
+    {
+        $rule = new CpfValidationRule();
+        $this->assertTrue($rule->passes('cpf', $cpf));
+    }
+
+    /**
+     * @dataProvider invalidCpfDataProvider
+     */
+    public function testInvalidCpf($cpf)
+    {
+        $rule = new CpfValidationRule();
+        $this->assertFalse($rule->passes('cpf', $cpf));
+    }
+
+    public function validCpfDataProvider()
+    {
+        return [
+            ['529.982.247-25'],
+            ['353.998.560-56'],
+            ['39053344705'],
+        ];
+    }
+
+    public function invalidCpfDataProvider()
+    {
+        return [
+            ['111.111.111-11'],
+            ['123.456.789-10'],
+            ['12345678900'],
+            [''],
+            [null],
+        ];
+    }
+}
